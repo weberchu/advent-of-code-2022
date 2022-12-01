8c9ad24570f48b31d4bde7b97782a713c3eb92bd
Weber Chu <weberchu@amazon.com>
2022-12-01 19:50:51 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,66 @@
 fun main() {
+    fun insertMaxHeap(maxHeap: MutableList<Int>, heapSize: Int, entry: Int) {
+        var inserted = false;
+        for (i in maxHeap.indices) {
+            if (entry > maxHeap[i]) {
+                maxHeap.add(i, entry)
+                inserted = true
+                break
+            }
+        }
+
+        if (!inserted) {
+            maxHeap.add(entry)
+        }
+
+        if (maxHeap.size > heapSize) {
+            maxHeap.removeAt(heapSize)
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = Int.MIN_VALUE
+        var totalCalories = 0
+        for (line in input) {
+            if (line.isEmpty()) {
+                if (totalCalories > maxCalories) {
+                    maxCalories = totalCalories
+                }
+                totalCalories = 0
+            } else {
+                val calories = line.toInt()
+                totalCalories += calories
+            }
+        }
+
+        if (totalCalories > maxCalories) {
+            maxCalories = totalCalories
+        }
+
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val maxCaloriesHeap = mutableListOf<Int>()
+        var totalCalories = 0
+        for (line in input) {
+            if (line.isEmpty()) {
+                insertMaxHeap(maxCaloriesHeap, 3, totalCalories)
+                totalCalories = 0
+            } else {
+                val calories = line.toInt()
+                totalCalories += calories
+            }
+        }
+
+        insertMaxHeap(maxCaloriesHeap, 3, totalCalories)
+
+        return maxCaloriesHeap.sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+//    val input = readInput("Test")
+
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
 }
---
+++ src/DayXX.kt
@@ -0,0 +1,14 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val input = readInput("Day01")
+
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
+}
